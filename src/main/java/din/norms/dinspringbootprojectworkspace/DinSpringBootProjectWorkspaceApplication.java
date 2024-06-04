65a3a95bcb16ebdd02e799fa419963abf6ccc70b
Steve Ngalamo <egoue2001@gmail.com>
2024-06-04 14:40:41 +0200

    core: started spring-boot project added

---
+++ src/main/java/din/norms/dinspringbootprojectworkspace/DinSpringBootProjectWorkspaceApplication.java
@@ -0,0 +1,13 @@
+package din.norms.dinspringbootprojectworkspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinSpringBootProjectWorkspaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinSpringBootProjectWorkspaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/din/norms/dinspringbootprojectworkspace/DinSpringBootProjectWorkspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package din.norms.dinspringbootprojectworkspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinSpringBootProjectWorkspaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
